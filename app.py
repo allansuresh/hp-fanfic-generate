c1b23cb98289893ca6c6add09110cfdea607b9d2
allansuresh <allansuresh9@gmail.com>
2024-11-10 02:27:11 +0530

    Render accessibility update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import subprocess
-import json
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +27,5 @@ def generate_story():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
